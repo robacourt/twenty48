f6af49adeac13ff1aee357a7de6ebb222795fd99
rob <rob.acourt@gmail.com>
2023-02-02 19:20:26 +0000

    Refactor row

--- lib/twenty48/row.ex
+++ lib/twenty48/row.ex
@@ -5,42 +5,35 @@ defmodule Twenty48.Row do
   def space, do: @space
   def obstacle, do: @obstacle
 
-  def slide_left(row), do: slide_left(row, [])
-
-  # Obstacles must stay in place so any spaces to the left of them
-  # must stay to the left of them and then continue to slide_left the
-  # rest of the tiles to the right
-  defp slide_left([@obstacle | tiles], spaces) do
-    spaces ++ [@obstacle | slide_left(tiles, [])]
+  def slide_left(tiles) do
+    tiles
+    |> Enum.chunk_by(&(&1 == @obstacle))
+    |> Enum.map(&slide_chunk_left/1)
+    |> Enum.concat()
   end
 
-  # Spaces can be skipped, but accumilate them so we can add them
-  # back later
-  defp slide_left([@space | tiles], spaces) do
-    slide_left(tiles, [@space | spaces])
+  defp slide_chunk_left([@obstacle | _] = obstacles), do: obstacles
+
+  defp slide_chunk_left(tiles) do
+    length = Enum.count(tiles)
+
+    tiles
+    |> remove_spaces()
+    |> merge_numbers()
+    |> pad_to_length(length)
   end
 
-  # Remove spaces to the right of the number tile in case there's another
-  # number tile that can be merged. Accumilate the spaces so we can add them
-  # back later
-  defp slide_left([number, @space | tiles], spaces) do
-    slide_left([number | tiles], [@space | spaces])
+  defp remove_spaces(tiles) do
+    Enum.reject(tiles, &(&1 == @space))
   end
 
-  # Two number tiles that have the same value can be merged
-  # into one number tile with double the value, creating an extra
-  # space that needs to be accumulated
-  defp slide_left([number, number | tiles], spaces) do
-    [number * 2 | slide_left(tiles, [@space | spaces])]
-  end
+  defp merge_numbers([number, number | tiles]), do: [2 * number | merge_numbers(tiles)]
+  defp merge_numbers([number | tiles]), do: [number | merge_numbers(tiles)]
+  defp merge_numbers([]), do: []
 
-  # Number tiles without a same value tile next to them
-  # are not merged
-  defp slide_left([number | tiles], spaces) do
-    [number | slide_left(tiles, spaces)]
-  end
+  defp pad_to_length(tiles, length) do
+    padding_lenth = length - Enum.count(tiles)
 
-  # We're at the right end of the row now,
-  # time to add back the spaces we removed from the left
-  defp slide_left([], spaces), do: spaces
+    tiles ++ List.duplicate(@space, padding_lenth)
+  end
 end
